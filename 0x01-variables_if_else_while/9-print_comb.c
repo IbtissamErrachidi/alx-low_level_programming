ce5f10839f86df438788577f3ca65ad18bd8131a
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-10 21:24:59 +0100

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+*main - entry point
+*
+*Description: prints all possible combinations of single-digit numbers.
+*
+*Return: 0 (success)
+*/
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	putchar(num + '0');
+
+	if (num < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
