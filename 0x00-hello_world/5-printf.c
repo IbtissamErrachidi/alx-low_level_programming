4369fbb68fd6c3adb5aed7e54f95484da463c1b7
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-10 18:33:57 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
 * main - Entry point
@@ -12,6 +12,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
