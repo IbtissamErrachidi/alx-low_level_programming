5ed73db976470ac9092b0c100f74ddc7e36b6aa1
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-10 10:54:22 +0100

    aaa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
 * main - entry point
 *
-* Description: false or true validation
+* Description: Check IF integer is positive or negative
 *
-* Return : 0 (success)
+* Return: Always 0 (success).
 */
 
 int main(void)
