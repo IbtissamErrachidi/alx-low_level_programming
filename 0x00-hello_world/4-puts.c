99e4adba72091515c9c4e137e09966817d35e7ed
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-10 14:00:35 +0100

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-gcc -E $CFILE > c
 
 /**
-* main - Entry point
+*main - Entry point
 *
-* Description: A C program that will print the message
-* "Programming is like building a multilingual puzzle" using the puts function.
+*Description: A C program that will print the message
+*"Programming is like building a multilingual puzzle" using the puts function.
 *
-* Return: Always 0 (success)
+*Return: Always 0 (success)
 */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	 puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
 
