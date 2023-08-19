8df44dd7f3ef3a070d04f36cd08dd136e077cd2b
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-08-18 18:49:38 -0700

    nm

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,19 +11,12 @@
 
 void print_square(int size)
 {
-	if (size <= 0)
+	int i, j;
+
+	for (int i = 0; i < size; i++)
 	{
+		for (int j = 0; j < size; j++)
+			_putchar('#');
 		_putchar('\n');
-	}
-	else
-	{
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
 	}
 }
