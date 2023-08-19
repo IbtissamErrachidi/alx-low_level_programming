d0bb09527eea6002dd69d0992b900404fa67192f
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-08-18 17:42:33 -0700

    nn

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,17 +14,19 @@ void print_triangle(int size)
 	int row, col;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-
-	for (row = 0; row < size; row++)
+	else
 	{
-		for (col = 0; col <= row; col++)
+		for (row = 1; row <= size; row++)
 		{
-			_putchar('#');
+			for (col = 1; col <= size; col++)
+			{
+				if ((row + col) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
