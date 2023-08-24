4e25ae2e5d752019fc334f9fb9ca4447f104032b
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-24 16:36:18 +0100

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using ROT13 cipher
+ * @str: input string
+ * Return: a pointer to the modified string
+ */
+
+char *rot13(char *str)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*str == rot13[i])
+			{
+				*str = ROT13[i];
+				break;
+			}
+		}
+	}
+	return (str);
+}
