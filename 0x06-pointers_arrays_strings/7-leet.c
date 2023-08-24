15db4179c235a09f620b37db741279126a27afe5
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-24 16:18:14 +0100

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: input string
+ * Return: a pointer to the modified string
+ */
+char *leet(char *str)
+{
+	char leet_letters[] = "aAeEoOtTlL";
+	char leet_replacements[] = "4433007711";
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_letters[j] != '\0'; j++)
+		{
+			if (str[i] == leet_letters[j])
+			{
+				str[i] = leet_replacements[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
