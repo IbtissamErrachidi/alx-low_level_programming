c749459d2053538a7dfda737795da410afa30ce5
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-30 13:31:52 +0100

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a number
+ * @n: number to calculate its factorial
+ * Return: -1 if number is negative, 0 if n is equal to 0, else factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
