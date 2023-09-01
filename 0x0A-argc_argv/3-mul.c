6331340bf11abfa629b8965811ca583c0fdaff4e
root <root@Ubuntu.myguest.virtualbox.org>
2023-09-01 19:48:30 +0100

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
 	int result = num1 * num2;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", result);
 
 	return (0);
