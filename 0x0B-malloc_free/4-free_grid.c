27d1d760ce91ac3719798c280e2264a555a4db84
root <root@Ubuntu.myguest.virtualbox.org>
2023-09-03 01:55:54 +0100

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+*free_grid - frees the mem allocate for the grid created by alloc_grid()
+*@grid: grid to free
+*@height: height of the grid
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+
+	if (grid == NULL || height == 0)
+		return;
+}
