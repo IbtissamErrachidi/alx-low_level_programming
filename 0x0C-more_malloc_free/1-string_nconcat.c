dfc6ad02e2d2182ba12bbd637a07f660dc8135e5
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-09-08 19:26:07 +0100

    3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-/**
-* *string_nconcat - function that concatenates two strings.
-* @s1: string to add to
-* @s2: string to concatenate from.
-* @n: number of bytes added from s2 to s1
-* Return: pointer to the outcome of concatenation, if failed then NULL.
-*/
 
+/**
+ * *string_nconcat - function that concatenates two strings.
+ * @s1: string to add to
+ * @s2: string to concatenate from.
+ * @n: number of bytes added from s2 to s1
+ * Return: pointer to the outcome of concatenation, if failed then NULL.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, j, l1 = strlen(s1), l2 = strlen(s2);
+	unsigned int i, j, l1, l2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
 	if (n >= l2)
 	{
 		n = l2;
-		ptr = malloc(sizeof(char) * (l1 + n + 1));
 	}
-	else
-		ptr = malloc(sizeof(char) * (l1 + n + 1));
+
+	ptr = malloc(sizeof(char) * (l1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; i < l1; i++)
 	{
 		ptr[i] = s1[i];
@@ -36,6 +39,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i++] = s2[j];
 	}
-	ptr[i++] = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
