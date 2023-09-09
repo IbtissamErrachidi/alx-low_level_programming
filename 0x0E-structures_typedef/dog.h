8ac167d9b66b302ecc7a97050f3e4215cf43fd5e
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-09-09 23:48:15 +0100

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - a struct about dogs idintification
+* @name: type char
+* @age: float type
+* @owner: char type
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
