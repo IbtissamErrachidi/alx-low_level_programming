21d9c009d823c4fc189f2a4d0da3c38519287162
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-10-02 22:14:52 +0100

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: The name of the file to create or modify.
+ * @text_content: The content to write into the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rw, len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+	rw = write(fd, text_content, len);
+	if (rw == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
