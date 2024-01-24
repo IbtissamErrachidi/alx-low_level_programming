403b8b49a77a5a7b33e085d4fc2bf68e9950a3fa
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-01-24 22:29:53 +0000

    update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+#include "hash_tables.h"
+=======
+>>>>>>> ea4615b121a6a39062a51b7cf029cb94a40a4129
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
